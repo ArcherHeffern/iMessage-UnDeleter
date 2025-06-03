71c68f8a67087ba14c01b61945651619dd2f2235
ArcherHeffern <heffernarcher@gmail.com>
2025-06-03 13:50:06 -0400

    pass all formatting checks and added if __name__==....

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+"""
+Watches iMessage database file and logs to file when a deleted message has been detected
+"""
+
+import sys
 from datetime import datetime
 from pathlib import Path
 from sqlite3 import connect
-import sys
 from time import sleep
 from typing import Optional
 
@@ -14,9 +18,7 @@ import helper as hp
 
 
 def eprint(msg):
-    """
-    Prints to stderr
-    """
+    """Prints msg to stderr"""
     print(msg, file=sys.stderr)
 
 
@@ -32,6 +34,8 @@ if not TARGET_EMAIL_OR_PHONE_NUMBER:
 
 
 class MessagesSchema(DataFrameModel):
+    """Represents messages between you and a target person"""
+
     message_id: Series[int]
     is_from_me: Series[bool]
     text_combined: Series[str] = Field(nullable=True)
@@ -45,12 +49,25 @@ class MessagesSchema(DataFrameModel):
 
 
 def get_chat(
-    imessage_file,
     target_email_or_phone_number: str,
     last_n: Optional[int] = None,
-    filter_your_messages: bool = False,
+    filter_messages_from_me: bool = False,
 ) -> DataFrame[MessagesSchema]:
-    conn = connect(imessage_file)
+    """Gets chat between you and a target email/phone number
+
+    Args:
+        target_email_or_phone_number (str): Other persons email or phone number.
+        * Phone number must be of format +12345678901
+        last_n (Optional[int], optional): How many messages to retrieve organized from most recent.
+        * None will return all messages.
+        * Defaults to None.
+        filter_messages_from_me (bool, optional): Filter out all messages from me.
+        * Defaults to False.
+
+    Returns:
+        DataFrame[MessagesSchema]: Dataframe of messages in thread
+    """
+    conn = connect(IMESSAGE_FILE)
 
     with conn as cur:
         handle = cur.execute(
@@ -66,7 +83,7 @@ def get_chat(
     if last_n:
         limit = f"LIMIT {last_n}"
     filter_your_messages_query = ""
-    if filter_your_messages:
+    if filter_messages_from_me:
         filter_your_messages_query = "AND is_from_me=0"
     messages = pd.read_sql_query(
         f"""select *,
@@ -80,27 +97,26 @@ def get_chat(
     )
     messages.rename(columns={"ROWID": "message_id"}, inplace=True)
 
-    messages["inferred_text"] = messages["attributedBody"].apply(
-        lambda x: hp.clean_text(x)
-    )
+    messages["inferred_text"] = messages["attributedBody"].apply(hp.clean_text)
     messages["text_combined"] = messages.apply(
         lambda row: row["inferred_text"] if pd.isnull(row["text"]) else row["text"],
         axis=1,
     )
     messages["message_effect"] = messages["expressive_send_style_id"].apply(
-        lambda x: hp.detect_message_effect(x)
+        hp.detect_message_effect
     )
     messages["is_thread_reply"] = (~messages["thread_originator_guid"].isnull()).astype(
         int
     )
-    messages["reaction"] = messages["associated_message_type"].apply(
-        lambda x: hp.detect_reaction(x)
-    )
-    messages["timestamp"] = messages["date_utc"].apply(lambda x: pd.Timestamp(x))
+    messages["reaction"] = messages["associated_message_type"].apply(hp.detect_reaction)
+    messages["timestamp"] = messages["date_utc"].apply(pd.Timestamp)
     # removing '\r' from the text of the messages as they interfere with the to_csv command.
     messages["text"] = messages["text"].str.replace("\r", "")
     messages["text_combined"] = messages["text_combined"].str.replace("\r", "")
     messages["inferred_text"] = messages["inferred_text"].str.replace("\r", "")
+    messages["is_from_me"] = messages["is_from_me"].apply(bool)
+    messages["is_audio_message"] = messages["is_audio_message"].apply(bool)
+    messages["is_thread_reply"] = messages["is_thread_reply"].apply(bool)
 
     columns = [
         "message_id",
@@ -114,13 +130,15 @@ def get_chat(
         "reaction",
         "is_thread_reply",
     ]
-    messages["is_from_me"] = messages["is_from_me"].apply(lambda x: bool(x))
-    messages["is_audio_message"] = messages["is_audio_message"].apply(lambda x: bool(x))
-    messages["is_thread_reply"] = messages["is_thread_reply"].apply(lambda x: bool(x))
     return MessagesSchema.validate(messages[columns])
 
 
 def log_messages(messages: DataFrame[MessagesSchema]):
+    """Logs messages to LOGFILE
+
+    Args:
+        messages (DataFrame[MessagesSchema]): Messages to be logged
+    """
     with open(LOGFILE, "a", encoding="utf-8") as f:
         f.write(f"==={datetime.now()}===\n")
         for _, message in messages.iterrows():
@@ -134,17 +152,18 @@ def log_messages(messages: DataFrame[MessagesSchema]):
             f.write(f"\tIs Thread Reply: {message["is_thread_reply"]}\n")
 
 
-print("Watching...")
-last_messages = get_chat(IMESSAGE_FILE, TARGET_EMAIL_OR_PHONE_NUMBER, 2, True)
+if __name__ == "__main__":
+    print("Watching...")
+    last_messages = get_chat(TARGET_EMAIL_OR_PHONE_NUMBER, 2, True)
 
-try:
-    while True:
-        new_messages = get_chat(IMESSAGE_FILE, TARGET_EMAIL_OR_PHONE_NUMBER, 2, True)
-        if not last_messages.equals(new_messages):
-            log_messages(last_messages)
-            print("Difference noticed...")
-        else:
-            sleep(0.5)
-        last_messages = new_messages
-except KeyboardInterrupt:
-    print("Exiting...")
+    try:
+        while True:
+            new_messages = get_chat(TARGET_EMAIL_OR_PHONE_NUMBER, 2, True)
+            if not last_messages.equals(new_messages):
+                log_messages(last_messages)
+                print("Difference noticed...")
+            else:
+                sleep(0.5)
+            last_messages = new_messages
+    except KeyboardInterrupt:
+        print("Exiting...")
